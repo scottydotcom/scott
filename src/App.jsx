ab88275533ac24c03ee71754f2c0947ef1d81880
scott <scottysora@gmail.com>
2025-05-20 00:09:00 -0400

    create intro page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Intro from "./components/Intro/Intro";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Intro />
     </div>
   );
 }
