69e44318269b0023fa9cce738bb67952da4f682f
scott <scottysora@gmail.com>
2025-06-04 22:31:09 -0400

    update intro nav link to home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import './navStyles.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="intro" smooth={true} duration={500}>Intro</Link>
+      <Link to="intro" smooth={true} duration={500}>Home</Link>
       <Link to="about" smooth={true} duration={500}>About</Link>
       <Link to="experience" smooth={true} duration={500}>Experience</Link>
       <Link to="project" smooth={true} duration={500}>Projects</Link>
